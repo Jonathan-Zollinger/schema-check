9c1b78118cb70ec1b4e21ceb6fc9a2ce978fe6f8
Jonathan Zollinger <jzollinger@trivir.com>
2023-08-15 14:34:39 -0600

    refactor: remove unused code

--- src/main/java/com/schemacheck/util/IdmTestUtils.java
+++ src/main/java/com/schemacheck/util/IdmTestUtils.java
@@ -27,24 +27,12 @@
  */
 package com.schemacheck.util;
 
-import com.schemacheck.model.Connector;
-import com.schemacheck.model.ConnectorAttribute;
-import com.schemacheck.model.IdmUnitTest;
 import com.schemacheck.model.Operation;
 import com.schemacheck.model.OperationData;
 
 import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 public class IdmTestUtils {
-    public static ConnectorAttribute[] getAttributes(Connector connector, String attributeName) {
-        return connector.getAttributes()
-                .stream()
-                .filter(connectorAttribute -> connectorAttribute.getName().equalsIgnoreCase(attributeName))
-                .toArray(ConnectorAttribute[]::new);
-    }
 
     public static String[] getAttributes(Operation operation, String attributeName) {
         return operation.getData().stream()
@@ -52,58 +40,6 @@ public class IdmTestUtils {
                 .map(OperationData::getValue)
                 .flatMap(Collection::stream)
                 .toArray(String[]::new);
-    }
-
-
-
-    public static List<String> getConnectorsNames(IdmUnitTest idmUnitTest) {
-        return idmUnitTest.getConnectors()
-                .stream()
-                .map(Connector::getName)
-                .collect(Collectors.toList());
-    }
-
-    public static Connector getConnector(IdmUnitTest idmUnitTest, String connectorName) {
-        Optional<Connector> optionalConnector = idmUnitTest.getConnectors().stream()
-                .filter(connector -> connector.getName().equalsIgnoreCase(connectorName))
-                .findFirst();
-        if (optionalConnector.isPresent()) {
-            return optionalConnector.get();
-        }
-        throw new IllegalArgumentException(String.format("Bad name requested. Cannot find '%s' connector", connectorName));
-    }
-
-    public static List<String> getAttributeNames(Connector connector) {
-        return connector.getAttributes()
-                .stream()
-                .map(ConnectorAttribute::getName)
-                .collect(Collectors.toList());
-    }
-
-    public static String stringify(OperationData operationData) {
-        return String.join("\n\t", new String[]{
-            "\tattribute: " + operationData.getAttribute(),
-            "value(s): <[" + String.join("]>, <[", operationData.getValue()) + "]>",
-            "OperationData meta: <[" + String.join("]>, <[", operationData.getMeta()) + "]>",
-        });
-    }
-
-
-    public static String stringify(Operation operation) {
-        return String.join("\n\t", new String[]{
-            "\tcomment: " + operation.getComment(),
-            "operation: " + operation.getOperation(),
-            "target: " + operation.getTarget(),
-            "waitInterval: " + operation.getWaitInterval(),
-            "retryCount: " + operation.getRetryCount(),
-            "disabled: " + operation.getDisabled(),
-            "failureExpected: " + operation.getFailureExpected(),
-            "critical: " + operation.getCritical(),
-            "repeatRange: " + operation.getRepeatRange(),
-            "data: " + operation.getData().stream().map(IdmTestUtils::stringify).collect(Collectors.joining("\n\t")),
-            "operation meta: " + String.join("\n\t", operation.getMeta())}
-        );
-
     }
 
 }
